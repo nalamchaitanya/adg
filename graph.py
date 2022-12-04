6e43c0a0d1000be370abc9912e086fee2045fd60
Sai Krishna Chaitanya Nalam Venkata Subrahmanya <nalakris@adobe.com>
2022-12-04 12:20:04 -0500

    Error in graph.py fixed

--- graph.py
+++ graph.py
@@ -1,12 +1,13 @@
-file1 = open('facebook.txt', 'r')
+file1 = open('facebook_combined.txt', 'r')
 Lines = file1.readlines()
 
 m = len(Lines);
 n = 0;
 for t in Lines:
     a = t.split(" ");
-    n = max(n, max(int(a[1]), int(a[1])));
+    n = max(n, max(int(a[0]), int(a[1])));
 
+n = n+1;
 print(str(n)+" "+str(m));
 
 for t in Lines:
