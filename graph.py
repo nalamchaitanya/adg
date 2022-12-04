1b99af0498c049ed56bdc3e2b8fc52050c786687
Sai Krishna Chaitanya Nalam Venkata Subrahmanya <nalakris@adobe.com>
2022-12-04 14:27:00 -0500

    Modified graph.py for github

--- graph.py
+++ graph.py
@@ -1,15 +1,31 @@
-file1 = open('facebook_combined.txt', 'r')
+# file1 = open('facebook_combined.txt', 'r')
+# Lines = file1.readlines()
+
+# m = len(Lines);
+# n = 0;
+# for t in Lines:
+#     a = t.split(" ");
+#     n = max(n, max(int(a[0]), int(a[1])));
+
+# n = n+1;
+# print(str(n)+" "+str(m));
+
+# for t in Lines:
+#     a = t.split(" ");
+#     print(str(int(a[0])+1)+" "+str(int(a[1])+1));
+
+file1 = open('github.dat', 'r')
 Lines = file1.readlines()
 
 m = len(Lines);
 n = 0;
 for t in Lines:
-    a = t.split(" ");
+    a = t.split(",");
     n = max(n, max(int(a[0]), int(a[1])));
 
 n = n+1;
 print(str(n)+" "+str(m));
 
 for t in Lines:
-    a = t.split(" ");
+    a = t.split(",");
     print(str(int(a[0])+1)+" "+str(int(a[1])+1));
